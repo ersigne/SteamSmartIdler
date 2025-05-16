f75813544dbc6e576a1b5a166d7e4fb940b3ac3e
Riccardo Signorello <r.sgnorello@gmail.com>
2025-05-16 20:22:26 +0200

    Organize constants in the new constants/ folder and Update references

---
+++ SteamSmartIdler/constants/enum.js
@@ -0,0 +1,16 @@
+const QueryRewardItems = Object.freeze({
+    CommunityItemClasses: Object.freeze({
+        'PROFILE_BACKGROUNDS': 3,
+        'EMOTICONS': 4,
+        'GAME_PROFILES': 8,
+        'ANIMATED_STICKERS': 11,
+        'CHAT_EFFECTS': 12,
+        'MINIPROFILE_BACKGROUNDS': 13,
+        'AVATAR_FRAMES': 14,
+        'ANIMATED_AVATAR': 15,
+        'STEAM_DECK_KEYBOARDS': 16,
+        'STEAM_DECK_STARTUP_MOVIE': 17
+    })
+});
+
+module.exports = { QueryRewardItems };
